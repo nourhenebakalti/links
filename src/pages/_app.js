e7b1c7c4ffd692506c7c80bc0f056ae4da997d78
Nourhene <nourhene.bakalti@esprit.tn>
2024-03-18 14:19:18 +0100

    favicon different sizes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,11 +10,31 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Links Station | Identity & Brand Development Services</title>
-        <link rel="icon" href="/img/favicon.ico" />
-        {/* Open Graph image metadata */}
-        <meta property="og:image" content="/img/smlogo.png" />
-      </Head>
+  <title>Links Station | Identity & Brand Development Services</title>
+  {/* Primary favicon */}
+  <link rel="icon" href="/img/favicon.ico" />
+
+  {/* Additional favicon sizes */}
+  <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="48x48" href="/img/favicon-48x48.png" />
+  {/* Add more sizes as needed */}
+
+  {/* SVG favicon */}
+  <link rel="icon" type="image/svg+xml" href="/img/favicon.svg" />
+
+  {/* Apple Touch Icon for iOS devices */}
+  <link rel="apple-touch-icon" sizes="57x57" href="/img/apple-touch-icon-57x57.png" />
+  <link rel="apple-touch-icon" sizes="72x72" href="/img/apple-touch-icon-72x72.png" />
+  {/* Add more sizes as needed */}
+
+  {/* Safari Pinned Tab Icon */}
+  <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#5bbad5" />
+
+  {/* Open Graph image metadata */}
+  <meta property="og:image" content="/img/smlogo.png" />
+</Head>
+
       <Cursor />
       <LoadingScreen />
       <ScrollToTop />
